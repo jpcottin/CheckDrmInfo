b81cd7472ce26cb92760b7c2a19e028dee361811
JP <jpcottin@gmail.com>
2025-09-24 20:32:13 -0700

    Changing theme

--- app/src/main/java/com/example/checkdrminfo/ui/theme/Color.kt
+++ app/src/main/java/com/example/checkdrminfo/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.checkdrminfo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange80 = Color(0xFFFFDAB9) // Light Orange
+val LightOrange80 = Color(0xFFF5E6CC) // Light Warm Grey
+val Terracotta80 = Color(0xFFE2A38A) // Light Terracotta
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Orange40 = Color(0xFFD2691E) // Dark Orange (Chocolate)
+val DarkOrange40 = Color(0xFF8B4513) // Dark Warm Grey (SaddleBrown)
+val DarkTerracotta40 = Color(0xFFA0522D) // Dark Terracotta (Sienna)
--- app/src/main/java/com/example/checkdrminfo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/checkdrminfo/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange80,
+    secondary = LightOrange80,
+    tertiary = Terracotta80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange40,
+    secondary = DarkOrange40,
+    tertiary = DarkTerracotta40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
